19f7dfe43431059ab90d915f2e2b50371f037d2a
Mathéo <matheo.galu56@gmail.com>
2022-01-09 12:39:19 -0500

    ok

--- unityProject/Assets/Scripts/FFPlaneMode/GenerateFire.cs
+++ unityProject/Assets/Scripts/FFPlaneMode/GenerateFire.cs
@@ -4,65 +4,105 @@ using UnityEngine;
 
 public class GenerateFire : MonoBehaviour
 {
-    private GameObject firePrefab;
+  private GameObject firePrefab;
 
-    private Vector3 center;
-    private Vector3 size;
+  private Vector3 center;
+  private Vector3 size;
 
-    private int nbMinOfFire = 4;
-    private int nbMaxOfFire = 8;
-    private bool chunckIsLoad = false;
+  private int nbMinOfFire = 4;
+  private int nbMaxOfFire = 8;
+  private bool chunckIsLoad = false;
 
-    private List<GameObject> fireList = new List<GameObject>();
+  private List<GameObject> fireList = new List<GameObject>();
 
-    private void FixedUpdate()
-    {
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position, Vector3.down);
+  void Start()
+  {
+    firePrefab = Resources.Load("Fire") as GameObject;
+    center = transform.position;
+    size = transform.localScale;
 
+    ChangeWaypoint();
+  }
+
+  private void FixedUpdate()
+  {
+    /*
+    RaycastHit hit;
+    Ray ray = new Ray(transform.position, Vector3.down);
+    
         bool grounded = Physics.Raycast(ray, out hit);
         if (grounded && hit.transform.tag.Equals("Ground"))
         {
-            Debug.Log("touch");
-            //FireSpawn();
-            if (!chunckIsLoad) chunckIsLoad = true;
+          Debug.Log("touch");
+          //FireSpawn();
+          if (!chunckIsLoad) chunckIsLoad = true;
         }
         else if (chunckIsLoad)
         {
-            chunckIsLoad = false;
-            FireUnspawn();
+          chunckIsLoad = false;
+          FireUnspawn();
         }
-    }
+    */
+  }
 
-    public void FireSpawn()
+  public void FireSpawn()
+  {
+    int nbOfFire = Random.Range(nbMinOfFire, nbMaxOfFire);
+    for (int i = 0; i < nbOfFire; ++i)
     {
-        int nbOfFire = Random.Range(nbMinOfFire, nbMaxOfFire);
-        for (int i = 0; i < nbOfFire; ++i)
-        {
-            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.z / 2, -size.z / 2));
-            GameObject fireCreated = Instantiate(firePrefab, pos, Quaternion.identity);
-            fireList.Add(fireCreated);
-        }
+      Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.z / 2, -size.z / 2));
+      GameObject fireCreated = Instantiate(firePrefab, pos, Quaternion.identity);
+      fireList.Add(fireCreated);
     }
+  }
 
-    public void ChangeWaypoint()
+  public void ChangeWaypoint()
+  {
+    int nbOfFireDisabled = 0;
+    foreach (GameObject fire in fireList)
     {
-        int nbOfFireDisabled = 0;
-        foreach (GameObject fire in fireList)
-        {
-            if (!fire.activeSelf)
-                nbOfFireDisabled++;
-        }
-
-        if (nbOfFireDisabled == fireList.Count)
-            Debug.Log("All the fire are desactived");
+      if (!fire.activeSelf)
+        nbOfFireDisabled++;
     }
 
-    public void FireUnspawn()
+    if (nbOfFireDisabled == fireList.Count)
     {
-        foreach (GameObject fire in fireList)
-        {
-            Destroy(fire);
-        }
+      Debug.Log("All the fire are desactived");
+
+      // Generate a new vector2 position between 1000 and 4000
+      Vector3 newPos = new Vector3(Random.Range(1000, 4000), 0.0f, Random.Range(1000, 4000));
+      Debug.Log(newPos);
+      float altitude = GetAltitude(new Vector2(transform.position.x, transform.position.z));
+      Debug.Log(altitude);
     }
+
+  }
+
+  public void FireUnspawn()
+  {
+    foreach (GameObject fire in fireList)
+    {
+      Destroy(fire);
+    }
+  }
+
+  public float GetAltitude(Vector2 pos)
+  {
+    float res = 0.0f;
+    // Raycast test
+    RaycastHit hit;
+    if (Physics.Raycast(new Vector3(pos.x, 10000, pos.y), Vector3.down, out hit, 50000))
+    {
+      float raycastDistance = hit.distance;
+      raycastDistance = 10000 - raycastDistance;
+      // Debug raycast result and mathematical result
+      Debug.Log("Raycast hit at " + hit.point + " with height " + raycastDistance);
+    }
+    else
+    {
+      Debug.LogError("Raycast missed");
+    }
+
+    return res;
+  }
 }
